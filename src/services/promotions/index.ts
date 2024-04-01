7a8977085297dc58c66ca7a5b3073c8893bfb42e
nuelkaiser <majekodunmiimmanuel9@gmail.com>
2024-04-01 05:57:21 -1000

    feat: all files pertaining to memberships added

---
+++ src/services/promotions/index.ts
@@ -0,0 +1,46 @@
+import { ApiResponse, Meta, getRequest } from "@/utils/api/calls";
+import type { IPromotion, PromotionFilter } from "./payload";
+
+const getMemberships = (
+  filter: PromotionFilter
+): Promise<
+  ApiResponse<{
+    Meta: Meta;
+    Promotions: IPromotion[];
+  }>
+> => {
+  const args = Object.keys(filter)
+    .map(
+      (item) =>
+        `${encodeURIComponent(item)}=${encodeURIComponent(
+          (filter as any)[item]
+        )}`
+    )
+    .join("&");
+  return getRequest<{ Meta: Meta; Promotions: IPromotion[] }>({
+    url: `/promotions/memberships?${args}`
+  });
+};
+
+const getCampaigns = (
+  filter: PromotionFilter
+): Promise<
+  ApiResponse<{
+    Meta: Meta;
+    Promotions: IPromotion[];
+  }>
+> => {
+  const args = Object.keys(filter)
+    .map(
+      (item) =>
+        `${encodeURIComponent(item)}=${encodeURIComponent(
+          (filter as any)[item]
+        )}`
+    )
+    .join("&");
+  return getRequest<{ Meta: Meta; Promotions: IPromotion[] }>({
+    url: `/promotions/campaigns?${args}`
+  });
+};
+
+export { getMemberships, getCampaigns };
