17430cc154bf88c92dbaea6c8c8b121c94337d37
Adeleke Oluwatosin <125203704+adeleketosin@users.noreply.github.com>
2024-05-05 16:09:31 +0100

    feat: Hotel Manage Information

--- src/app/hotels/[idOrSlug]/manage/management-information.tsx
+++ src/app/hotels/[idOrSlug]/manage/management-information.tsx
@@ -2,14 +2,27 @@ import ManagerForm from "@/components/ManagementInformation/ManagerForm";
 import ToastWrapper from "@/components/toast/Toast";
 import { toastIcons } from "@/utils/constants";
 import toast from "react-hot-toast";
-import { useMutation } from "react-query";
-import HotelAdminLayout from "@/layout/hotelAdmin/HotelAdmin";
+import { useMutation, useQuery } from "react-query";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import React from "react";
-import { ManagerInformationPayload } from "@/services/hotel/payload";
-import { updateHotelManagerInformation } from "@/services/hotel";
+import { IHotel, ManagerInformationPayload } from "@/services/hotel/payload";
+import { getHotel, updateHotelManagerInformation } from "@/services/hotel";
+import { useParams } from "next/navigation";
+import queryKeys from "@/utils/api/queryKeys";
 
 const ManagerInformation = () => {
-  const hotel = useSelector(getStateHotel);
+  
+  const { idOrSlug } = useParams<{ idOrSlug: string }>();
+
+  const {data } = useQuery(
+    [queryKeys.getHotelByID],
+    () => getHotel(idOrSlug?.toString()),
+    {
+      enabled: !!idOrSlug // Would only make this request if slug is truthy
+    }
+  );
+
+  const hotel = data?.data as IHotel;
 
   const { Manager } = hotel?.CooperateInformation || {};
 
@@ -25,11 +38,11 @@ const ManagerInformation = () => {
     });
   };
   return (
-    <HotelAdminLayout>
+    <DefaultLayout>
       <div className="w-full max-w-[600px]">
         <ManagerForm onSubmit={onSubmit} manager={Manager} isSubmitting={isLoading} />
       </div>
-    </HotelAdminLayout>
+    </DefaultLayout>
   );
 };
 
