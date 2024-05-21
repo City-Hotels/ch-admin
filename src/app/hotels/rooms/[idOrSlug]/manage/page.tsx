7a01d6e1ba6d4d6d79f551630051f709b7b3f5e0
Bluewizzy1 <125451191+Bluewizzy1@users.noreply.github.com>
2024-05-21 23:46:40 +0100

    feat: Manage-Room-Page

---
+++ src/app/hotels/rooms/[idOrSlug]/manage/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import  ManageApartment  from "@/components/ManageApartment/ManageApartment";
+import { useParams } from "next/navigation";
+
+import React from "react";
+
+const Index: React.FC = () => {
+  const { idOrSlug } = useParams<{ idOrSlug: string }>();
+  return (
+    <DefaultLayout>
+      <>
+        {/* <ManageApartment apartmentid={idOrSlug} /> */}
+      </>
+    </DefaultLayout>
+  );
+};
+
+export default Index;
