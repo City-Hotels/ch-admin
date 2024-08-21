8c0a8d40b95046e65c307d3c3046d2877220dfd6
olaoluphemy <bidesim8@gmail.com>
2024-08-21 19:25:51 +0100

    fix: wrapped layout component with websocket context

--- src/app/ticket/[idOrSlug]/page.tsx
+++ src/app/ticket/[idOrSlug]/page.tsx
@@ -2,18 +2,15 @@
 
 import ChatLayout from "@/components/ChatLayout/Chat";
 import UserChat from "@/components/Ticket/user-chat";
-import { WebSocketProvider } from "@/context/WebSocketContext";
 import { useState } from "react";
 
 const ChatPage = () => {
   const [chat, setChat] = useState<"ticket" | "support">("support");
 
   return (
-    <WebSocketProvider>
-      <ChatLayout active={chat} setActive={setChat}>
-        <UserChat showConversation />
-      </ChatLayout>
-    </WebSocketProvider>
+    <ChatLayout active={chat} setActive={setChat}>
+      <UserChat showConversation />
+    </ChatLayout>
   );
 };
 
--- src/app/ticket/page.tsx
+++ src/app/ticket/page.tsx
@@ -2,7 +2,6 @@
 
 import ChatLayout from "@/components/ChatLayout/Chat";
 import { useState } from "react";
-import { WebSocketProvider } from "@/context/WebSocketContext";
 
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
@@ -12,11 +11,9 @@ const ChatPage = () => {
   const [chat, setChat] = useState<"ticket" | "support">("ticket");
 
   return (
-    <WebSocketProvider>
-      <ChatLayout active={chat} setActive={setChat}>
-        <UserChat />
-      </ChatLayout>
-    </WebSocketProvider>
+    <ChatLayout active={chat} setActive={setChat}>
+      <UserChat />
+    </ChatLayout>
   );
 };
 
